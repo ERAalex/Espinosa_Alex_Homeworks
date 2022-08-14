e55a5daa50f3bfe7b925da6080d25de386384f43
Alex_ERA <eraspb@mail.ru>
2022-08-14 12:28:17 +0300

    best way to task 5

--- Task2/task3.py
+++ Task2/task3.py
@@ -2,6 +2,8 @@
 all_files = ['1.txt', '2.txt', '3.txt']
 dict_text = {}
 size_text = {}
+all_text = []
+
 for item in all_files:
     with open(item, encoding='utf-8') as f:
         first_txt = f.read().split('\n')
@@ -11,7 +13,6 @@ for item in all_files:
         size_text[size] = item
 
 sorted_size_dic = dict(sorted(size_text.items()))
-print(sorted_size_dic)
 
 with open('result.txt', 'a', encoding='utf-8') as f:
     for key, value in sorted_size_dic.items():
