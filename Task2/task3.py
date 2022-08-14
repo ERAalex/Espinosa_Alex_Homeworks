ceaeadfe24f53e1d80df80025c0f0b2daa42d0ff
Alex_ERA <eraspb@mail.ru>
2022-08-14 12:24:28 +0300

    best way to task 4

--- Task2/task3.py
+++ Task2/task3.py
@@ -1,79 +1,23 @@
 
-text_1 = '1.txt'
-text_2 = '2.txt'
-text_3 = '3.txt'
+all_files = ['1.txt', '2.txt', '3.txt']
+dict_text = {}
+size_text = {}
+for item in all_files:
+    with open(item, encoding='utf-8') as f:
+        first_txt = f.read().split('\n')
+        size = len(first_txt)
+        all_text.append(item)
+        dict_text[size] = first_txt
+        size_text[size] = item
 
-# вытаскиваем информацию из 1 файла
-with open(text_1, encoding='utf-8') as f:
-    first_txt = f.read().split('\n')
+sorted_size_dic = dict(sorted(size_text.items()))
+print(sorted_size_dic)
+
+with open('result.txt', 'a', encoding='utf-8') as f:
+    for key, value in sorted_size_dic.items():
+        f.write(str(key) + '\n' + value + '\n')
+        for k, value in dict_text.items():
+            if k == key:
+                for item in value:
+                    f.write(item + '\n')
     f.close()
-
-# вытаскиваем информацию из 1 файла
-with open(text_2, encoding='utf-8') as f:
-    second_txt = f.read().split('\n')
-    f.close()
-
-# вытаскиваем информацию из 1 файла
-with open(text_3, encoding='utf-8') as f:
-    third_txt = f.read().split('\n')
-    f.close()
-
-size_first = len(first_txt)
-size_second = len(second_txt)
-size_third = len(third_txt)
-
-
-if size_first <= size_second and size_first <= size_third:
-    with open('result.txt', 'a', encoding='utf-8') as f:
-        f.write(text_1 + '\n' + str(size_first) + '\n')
-        for item in first_txt:
-            f.write(item + '\n')
-
-        if size_second <= size_third:
-            f.write(text_2 + '\n' + str(size_second) + '\n')
-            for item in second_txt:
-                f.write(item + '\n')
-            f.write(text_3 + '\n' + str(size_third) + '\n')
-            for item in third_txt:
-                f.write(item + '\n')
-        f.close()
-
-
-
-if size_second <= size_first and size_second <= size_third:
-    with open('result.txt', 'a', encoding='utf-8') as f:
-        f.write(text_2 + '\n' + str(size_second) + '\n')
-        for item in second_txt:
-            f.write(item + '\n')
-
-        if size_first <= size_third:
-            f.write(text_1 + '\n' + str(size_first) + '\n')
-            for item in first_txt:
-                f.write(item + '\n')
-            f.write(text_3 + '\n' + str(size_third) + '\n')
-            for item in third_txt:
-                f.write(item + '\n')
-        f.close()
-else:
-    with open('result.txt', 'a', encoding='utf-8') as f:
-        f.write(text_3 + '\n' + str(size_third) + '\n')
-        for item in third_txt:
-            f.write(item + '\n')
-
-        if size_first <= size_second:
-            f.write(text_1 + '\n' + str(size_first) + '\n')
-            for item in first_txt:
-                f.write(item + '\n')
-            f.write(text_2 + '\n' + str(size_second) + '\n')
-            for item in second_txt:
-                f.write(item + '\n')
-        else:
-            f.write(text_2 + '\n' + str(size_second) + '\n')
-            for item in second_txt:
-                f.write(item + '\n')
-            f.write(text_1 + '\n' + str(size_first) + '\n')
-            for item in first_txt:
-                f.write(item + '\n')
-        f.close()
-
-
