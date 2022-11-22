bbe9d5a0a2830e00993182eca98771280b0f6bb6
Alex_ERA <107221974+ERAalex@users.noreply.github.com>
2022-11-22 22:34:08 +0300

    Update task_1.py

--- 6_test/task_1.py
+++ 6_test/task_1.py
@@ -46,7 +46,7 @@ def get_unique_list(some_dict):
 
 
 
-
+#
 ####################################__EXAMPLE_3__####################################
 
 stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
